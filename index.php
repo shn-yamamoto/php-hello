a66593fddce683e11966a8074d49cd7e6a750c73
shn-yamamoto <shingo.yamamoto.zr@hitachi-solutions.com>
2017-12-06 01:13:11 +0000

    change message

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body bgcolor="yellow">
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Wellcome to OpenShift Container Platform</p>'; ?> 
  <h1>
  <?php echo getenv('INFRA_ENV'); ?>
  </h1>
