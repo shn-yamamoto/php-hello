5c7bcc309580f547c5896162aab5d2e3ae58ff01
shn-yamamoto <shingo.yamamoto.zr@hitachi-solutions.com>
2017-12-08 07:54:01 +0000

    to blue

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
   <title>PHP Test</title>
  </head>
- <body bgcolor="yellow">
+ <body bgcolor="blue">
  <?php echo '<p>Welcome to OSC 2017 .Enterprise</p>'; ?> 
  <h1>
  <?php echo getenv('INFRA_ENV'); ?>
